5a71e48b7bb0b0699cef1136403a0d0dae845341
alexs <alexs@LAPTOP-HBVCATHO>
2021-10-01 10:23:11 -0400

    Add project files.

---
+++ WordUnscrambler/FileReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WordUnscrambler
+{
+    class FileReader
+    {
+        public string[] Read(string filename)
+        {
+           
+
+            return null;
+
+        }
+    }
+}
